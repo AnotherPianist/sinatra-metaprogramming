773f51262524bf7f2c52defcbb4b7fa4b009e92f
ThePianist <cafarimo@outlook.com>
2023-06-26 23:25:23 -0400

    Define paths

--- my_sinatra.rb
+++ my_sinatra.rb
@@ -1,4 +1,26 @@
 # frozen_string_literal: true
 
-class MySinatra
+module MySinatra
+  def self.included(klass)
+    puts "MySinatra included in #{klass}"
+  end
+
+  def run(path)
+    if paths.key? path
+      paths[path].call
+    else
+      StandardError.new "No route defined for #{path}"
+    end
+  end
+
+  def paths
+    @paths ||= {}
+  end
+
+  def get(path, &block)
+    puts "Defining a GET request for #{path}"
+    paths[path] = block
+  end
 end
+
+include MySinatra
--- runner.rb
+++ runner.rb
@@ -1,2 +1,9 @@
 # frozen_string_literal: true
 
+require_relative 'my_sinatra'
+
+get '/hello' do
+  puts 'Hello world!'
+end
+
+run '/hello'
